a108f39caee35c7793b4ba1755471a349d965e54
Mat Werber <mat@releasehub.com>
2022-05-16 03:36:54 -0700

    update

--- run-local.sh
+++ run-local.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Spins up a local SQL Server Express container and a container instance of 
+# mssql-tools (aka sql-runner), then opens a terminal session with the the
+# tools container so you can test your scripts. 
+docker compose build
 docker compose -f docker-compose.yml -f docker-compose.local.yml up -d
-
 docker exec -it sql-runner bash
